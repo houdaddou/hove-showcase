ef54ef66b6016c3634529d814d059aee7c16e421
haddou <houda.addou@kisio.com>
2023-06-12 16:52:25 +0200

    fix

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import { Link } from 'gatsby'
 import React from 'react'
 import * as classes from './style.module.css';
+import Button from '@mui/material/Button';
 
 
 export default function Navbar() {
@@ -16,7 +17,9 @@ export default function Navbar() {
                 <Link to="/about">QUI SOMMES-NOUS?</Link>
                 <Link to="/projects">ACTUALITES</Link>  
                 <Link to="/projects">CONTACT</Link>   
-                <Link to="/projects">RELEASE NOTES</Link>     
+                <Link to="/projects">RELEASE NOTES</Link>
+                <Button  size="small"><div className={classes.TranslateFr}>FR</div></Button>
+                <Button size="small"><div className={classes.TranslateEn}>EN</div></Button>
             </div>
         </div>
     </header>
--- src/pages/solutions/index.js
+++ src/pages/solutions/index.js
@@ -1,16 +1,31 @@
 import * as React from "react"
-import Navbar from '../../components/Navbar'
+import Layout from "../../components/Layout"
+import * as styles from "../../styles/solutions.module.css"
+import Card from './cardframe';
+
 
 
 export default function Solutions() {
   return (
+    <Layout>
     <section>
-        <Navbar />
-      <div>
-        <h2>NOS SOLUTIONS</h2>
-        <h3>Navitia</h3>
-        <h4>Le calculateur d'itinéraire</h4>
+      <div className={styles.header}>
+        <h1>NOS SOLUTIONS</h1>
+      </div>
+      <div className={styles.header1}>
+        <h2>Navitia</h2>
+        <h3>Le calculateur d'itinéraire</h3>
+        <Card />
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+      </div>
+      <div  className={styles.header2}>
+      <h2>Patterns</h2>
+        <h3>Les études de territoire</h3>
       </div>
     </section>
+    </Layout>
   )
 }
